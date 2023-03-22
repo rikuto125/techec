5f1669d33d1adb234fff3d0f80172380680b8d22
iwairikuto <wjrikuto@gmial.com>
2023-03-22 12:56:14 +0900

    修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import "../../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { extendTheme } from "@chakra-ui/react";
-import chkraTheme from "../../chkraTheme";
+import chkraTheme from "../../chakraTheme";
 
 const theme = extendTheme(chkraTheme);
 export default function App({ Component, pageProps }: AppProps) {
